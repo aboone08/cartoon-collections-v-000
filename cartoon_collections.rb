523ee6820d9b83a384228c1958235fcab60dab20
Andranique Boone <andranique.boone@gmail.com>
2019-02-26 23:23:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planeteers.collect(&:capitalize) << planeteer_calls
+  planeteer_calls.collect(&:capitalize) << planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
