131624c069f5372725a0f34f67fcfbd615436d7c
Andranique Boone <andranique.boone@gmail.com>
2019-02-26 23:20:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|name, i|
+    i+=1
+    puts "#{i}. #{name}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteers = [" "]
+  planeteers.collect(&:capitalize) << planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
