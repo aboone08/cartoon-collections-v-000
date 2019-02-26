ed016a538431bfb8a7ffd1da8b636227e0b4606c
Andranique Boone <andranique.boone@gmail.com>
2019-02-26 23:50:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(calls)
 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.detect{|snacks| cheese_types.include? snacks}
 end
